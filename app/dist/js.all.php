4f4a01a27d60e4bfedacee40e66b18e9bda8e875
Kharis Wijayanto <real.kharis.w@gmail.com>
2019-10-07 21:11:37 +0700

    #0

---
+++ app/dist/js.all.php
@@ -0,0 +1,7 @@
+	<!-- Essential javascripts for application to work-->
+    <script src="../dist/js/jquery-3.2.1.min.js"></script>
+    <script src="../dist/js/popper.min.js"></script>
+    <script src="../dist/js/bootstrap.min.js"></script>
+    <script src="../dist/js/main.js"></script>
+    <!-- The javascript plugin to display page loading on top-->
+    <script src="../dist/js/plugins/pace.min.js"></script>
---
+++ app/dist/left-nav.php
@@ -0,0 +1,67 @@
+    <aside class="app-sidebar">
+      <div class="app-sidebar__user"><img class="app-sidebar__user-avatar" src="../assets/img/user-default.png" alt="User Image" width="50">
+        <div>
+          <table>
+            <tr>
+              <td class="app-sidebar__user-name" style="max-width: 140px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">
+                Muhammad Sholehudin
+              </td>
+            </tr>
+            <tr>
+              <td class="app-sidebar__user-designation" style="max-width: 140px; overflow: hidden; text-overflow: ellipsis; white-space: nowrap;">
+                Pejabat Administrasi Rumah Tangga
+              </td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <ul class="app-menu">
+        <li>
+          <a class="app-menu__item" href="index.html"><i class="app-menu__icon fa fa-home fa-lg"></i><span class="app-menu__label">&nbsp;Dashboard</span></a>
+        </li>
+        <li class="treeview">
+          <a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-walking fa-lg"></i><span class="app-menu__label">&nbsp;Kegiatan</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+          <ul class="treeview-menu">
+            <li>
+              <a class="treeview-item" href="bootstrap-components.html"><i class="icon fa fa-circle fa-sm"></i> Tahunan</a>
+            </li>
+            <li>
+              <a class="treeview-item" href="https://fontawesome.com/v4.7.0/icons/" target="_blank" rel="noopener"><i class="icon fa fa-circle fa-sm"></i>Bulanan</a>
+            </li>
+            <li>
+              <a class="treeview-item" href="ui-cards.html"><i class="icon fa fa-circle fa-sm"></i>Harian</a>
+            </li>
+            <li>
+              <a class="treeview-item" href="ui-cards.html"><i class="icon fa fa-circle fa-sm"></i>Tambahan</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a class="app-menu__item" href="index.html"><i class="app-menu__icon fa fa-pen fa-lg"></i><span class="app-menu__label">&nbsp;Produktifitas</span></a>
+        </li>
+        <li class="treeview">
+          <a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-file fa-lg"></i><span class="app-menu__label">&nbsp;Laporan</span><i class="treeview-indicator fa fa-angle-right"></i></a>
+          <ul class="treeview-menu">
+            <li>
+              <a class="treeview-item" href="bootstrap-components.html"><i class="icon fa fa-circle fa-sm"></i> Laporan Tahunan</a>
+            </li>
+            <li>
+              <a class="treeview-item" href="#" target="_blank" rel="noopener"><i class="icon fa fa-circle fa-sm"></i>Laporan Bulanan</a>
+            </li>
+            <li>
+              <a class="treeview-item" href="ui-cards.html"><i class="icon fa fa-circle fa-sm"></i>Laporan Jumlah Kegiatan</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a class="app-menu__item" href="#"><i class="app-menu__icon fa fa-book fa-lg"></i><span class="app-menu__label">&nbsp;Panduan Pegawai</span></a>
+        </li>
+        <hr style="background-color: white; width: 80%;">
+        <li>
+          <a class="app-menu__item" href="#"><i class="app-menu__icon fa fa-list fa-lg"></i><span class="app-menu__label">&nbsp;Data Pegawai</span></a>
+        </li>
+        <li>
+          <a class="app-menu__item" href="#"><i class="app-menu__icon fa fa-undo-alt fa-lg"></i><span class="app-menu__label">&nbsp;Backup And Restore</span></a>
+        </li>
+      </ul>
+    </aside>
