aafa20bba0d792a9c688366f817519880d47a1f6
Kharis Wijayanto <real.kharis.w@gmail.com>
2019-10-08 07:21:01 +0700

    Initial commit

--- app/dist/js.all.php
+++
@@ -1,7 +0,0 @@
-	<!-- Essential javascripts for application to work-->
-    <script src="../dist/js/jquery-3.2.1.min.js"></script>
-    <script src="../dist/js/popper.min.js"></script>
-    <script src="../dist/js/bootstrap.min.js"></script>
-    <script src="../dist/js/main.js"></script>
-    <!-- The javascript plugin to display page loading on top-->
-    <script src="../dist/js/plugins/pace.min.js"></script>
