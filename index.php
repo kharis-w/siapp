a3aa9b6517533de76ec2ce23b859c2dd348fdbad
Kharis Wijayanto <44442658+kharis-w@users.noreply.github.com>
2020-01-04 17:51:08 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
-<?php
-header('location:  app/login.php');
-?>
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>HTML Meta Tag</title>
+      <meta http-equiv = "refresh" content = "2; url = app/login.php" />
+   </head>
+   <body>
+   </body>
+</html>
