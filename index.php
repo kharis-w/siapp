4952d59702815df277807862061a9d26405176d5
Kharis Wijayanto <44442658+kharis-w@users.noreply.github.com>
2020-01-04 17:52:41 +0700

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-   <head>
-      <title>HTML Meta Tag</title>
-      <meta http-equiv = "refresh" content = "2; url = app/login.php" />
-   </head>
-   <body>
-   </body>
-</html>
